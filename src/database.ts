5b5b5084d1cfac0091f0ffc21d77f4d2e05416d9
Ahmad Malik <arma7x@live.com>
2023-03-27 18:46:40 +0800

    WIP: PriceCatcher

--- src/database.ts
+++ src/database.ts
@@ -3,6 +3,63 @@ const sqlite3 = require("sqlite3").verbose();
 
 const databaseInstance = new sqlite3.Database(path.join(process.cwd(), "pricecatcher.db"));
 
+export type NestedObject = {
+  [key: string]: any;
+}
+
+function getItemGroups(db: sqlite3.Database): Array<String> {
+  let groups = [];
+  const stmt = `SELECT item_group, COUNT(item_group) AS total from items
+  WHERE item_group !='UNKNOWN'
+  GROUP BY item_group
+  ORDER BY item_group ASC`;
+  db.serialize(() => {
+    db.each(stmt, (err, row) => {
+      groups.push(row.item_group);
+    });
+  });
+  return groups;
+}
+
+function getItemCategories(db: sqlite3.Database): Array<String> {
+  let categories = [];
+  const stmt = `SELECT item_category, COUNT(item_category) AS total from items
+  WHERE item_category !='UNKNOWN'
+  GROUP BY item_category
+  ORDER BY item_category ASC`;
+  db.serialize(() => {
+    db.each(stmt, (err, row) => {
+      categories.push(row.item_category);
+    });
+  });
+  return categories;
+}
+
+function getPremisesNestedLocations(db: sqlite3.Database): NestedObject {
+  let nested: NestedObject = {};
+  const stmt = `SELECT state, district, premise_type from premises
+  WHERE premise_code!=-1
+  GROUP BY state, district, premise_type
+  ORDER BY state ASC`;
+  db.serialize(() => {
+    db.each(stmt, (err, row) => {
+      if (nested[row.state] == null)
+        nested[row.state] = {};
+      if (nested[row.state][row.district] == null)
+        nested[row.state][row.district] = [];
+      nested[row.state][row.district].push(row.premise_type);
+    });
+  });
+  return nested;
+}
+
+const itemGroups = getItemGroups(databaseInstance);
+const itemCategories = getItemCategories(databaseInstance);
+const premisesNestedLocations = getPremisesNestedLocations(databaseInstance);
+
 export {
   databaseInstance,
+  itemGroups,
+  itemCategories,
+  premisesNestedLocations,
 }
